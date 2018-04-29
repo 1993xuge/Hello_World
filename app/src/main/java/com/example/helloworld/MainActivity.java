e19683102c6898979330dc8e737d5cdad9724d90
wangyixu <wangyixu@wangyixudeMacBook-Pro.local>
2018-04-29 21:15:41 +0800

    hanhan

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         /*     xixhahaxixixhha   */
 
-        Log.d(TAG, "onCreate: hahahhahahahha");
+        Log.d(TAG, "oate: hahahhahahahha");
 
     }
 }
