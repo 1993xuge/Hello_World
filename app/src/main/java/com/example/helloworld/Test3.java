dc640d31415faa9c08dee609c095df3caf52b7ec
yixu.wang <yixu.wang@ihandysoft.com>
2018-04-30 08:39:49 +0800

    xugess

--- app/src/main/java/com/example/helloworld/Test3.java
+++ app/src/main/java/com/example/helloworld/Test3.java
@@ -8,6 +8,5 @@ package com.example.helloworld;
 
 public class Test3 {
     public static final String TAG = Test3.class.getSimpleName();
-    public static final String TAG2 = Test3.class.getSimpleName();
 
 }
